4892958e2c75a8074d98b6a86000da9d0307be0f
Luís Gaspar da Cruz <81656680+LuisGaspar182@users.noreply.github.com>
2022-09-12 18:34:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var i = document.getElementById("name");
+var texto = document.getElementById("texto");
+const hellotxt = document.getElementById("hello");
+const a =
+"https://canalsolar.com.br/wp-content/uploads/2021/11/UNIFEI-vai-receber-E-5-milhoes-para-construir-usina-de-hidrogenio.jpg";
+const b =
+"https://defatoonline.com.br/wp-content/uploads/2020/04/Unifei.jpg";
+function addvalue() {
+if (i.src === a) {
+    i.src = b;
+} else {
+    i.src = a;
+}
+if (texto.textContent === "Situado em Itajuba-MG") {
+    texto.textContent = "Situdo em Itabira-MG"
+} else {
+    texto.textContent = "Situado em Itajuba-MG"
+}
+}
